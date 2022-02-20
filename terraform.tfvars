58c8e009bac60ef9f62dd050709a5b3805da70dd
cbuxton1984 <cbuxton1984@gmail.com>
2022-02-20 20:08:27 +0000

    Added foreach and count

--- terraform.tfvars
+++ terraform.tfvars
@@ -15,10 +15,7 @@ rg_name = "cb-pipelinedemo"
 
 vnet_name = "cb-vnet"
 
-vnet_address_space = ["10.0.0.0/16"]
-
 # Subnet
 
 subnet_name = "cb-subnet"
 
-subnet_address_prefixes = ["10.0.1.0/24"]
--- variables.tf
+++ variables.tf
@@ -24,19 +24,9 @@ variable "vnet_name" {
       type = string
 }
 
-variable "vnet_address_space" {
-      description = "vNet Address Space"
-      type = list
-}
-
 # Subnet
 
 variable "subnet_name" {
       description = "Subnet Name"
       type = string
-}
-
-variable "subnet_address_prefixes" {
-      description = "Subnet Prefixes"
-      type = list
 }
